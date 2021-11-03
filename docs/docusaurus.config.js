2721f709f078d8d4a6abd70ae83d0c6aee0a74e6
Tim Crooker <timothycrooker@gmail.com>
2021-11-02 22:44:49 -0400

    edit config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
 	title: 'My Site',
 	tagline: 'Dinosaurs are cool',
-	url: 'https://your-docusaurus-test-site.com',
-	baseUrl: '/',
+	url: 'https://projenerator.github.io',
+	baseUrl: '/projenerator/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'docusaurus', // Usually your repo name.
+	organizationName: 'TimCrooker', // Usually your GitHub org/user name.
+	projectName: 'projenerator', // Usually your repo name.
 
 	presets: [
 		[
