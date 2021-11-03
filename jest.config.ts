2471514bf0ae4a32897808b58ea4cbaba58730af
Tim Crooker <timothycrooker@gmail.com>
2021-11-03 12:32:45 -0400

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
 	verbose: true,
 	testEnvironment: 'node',
-	setupTestFrameworkScriptFile: './tests/setup.js',
 	modulePathIgnorePatterns: ['dist'],
 }
 export default config
