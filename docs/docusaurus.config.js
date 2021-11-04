beaafe21bd58602b250c4150a525e93b36b21293
Tim Crooker <timothycrooker@gmail.com>
2021-11-03 20:03:04 -0400

    hopefully fix documentation site url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
 	title: 'Grit',
 	tagline: 'Generate really interesting tech',
-	url: 'https://timcrooker.github.io/grit/',
+	url: 'https://timcrooker.github.io',
 	baseUrl: '/grit/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
