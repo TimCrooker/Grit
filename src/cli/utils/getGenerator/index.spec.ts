15c57377d2c41c8e5453e0a708518de5d50e9c11
Tim Crooker <timothycrooker@gmail.com>
2021-12-02 11:47:55 -0500

    test repairs

--- src/cli/utils/getGenerator/index.spec.ts
+++ src/cli/utils/getGenerator/index.spec.ts
@@ -1,24 +1,41 @@
+import { defaultGeneratorFile } from '@/generator/generatorConfig'
 import { parseGenerator } from '@/utils/parseGenerator'
+import path from 'path'
 import { getGenerator, loadGeneratorGrit } from '.'
 
 describe('Name of the group', () => {
 	it('should load npm generator versioned instance', async () => {
-		const parsedGenerator = parseGenerator('generator')
+		const parsedGenerator = parseGenerator(path.resolve(__dirname, 'fixtures'))
 
 		const generator = await loadGeneratorGrit(parsedGenerator)
 
 		const grit = new generator({
-			config: {},
+			config: defaultGeneratorFile,
 			generator: parsedGenerator,
 			mock: true,
 		})
 
 		expect(grit).toBeDefined()
+
+		await grit.run()
+
+		const output = await grit.readOutputFile('foo.txt')
+
+		expect(output).toBe('foo')
 	})
 
 	it('should load instantiated generator', async () => {
-		const grit = await getGenerator({ generator: 'generator', mock: true })
+		const grit = await getGenerator({
+			generator: path.resolve(__dirname, 'fixtures'),
+			mock: true,
+		})
 
 		expect(grit).toBeDefined()
+
+		await grit.run()
+
+		const output = await grit.readOutputFile('foo.txt')
+
+		expect(output).toBe('foo')
 	})
 })
