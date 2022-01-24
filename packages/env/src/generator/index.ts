ed6f41a1abb99cc71aa0263646a3c4befa5a5eab
Tim Crooker <timothycrooker@gmail.com>
2022-01-24 14:57:26 -0500

    fix log

--- packages/env/src/generator/index.ts
+++ packages/env/src/generator/index.ts
@@ -178,7 +178,7 @@ class Grit {
 		// Set log level from run mode
 		if (opts.debug && !this.opts.logLevel) {
 			this.opts.logLevel = 4
-		} else if (opts.silent) {
+		} else if (opts.silent && !this.opts.logLevel) {
 			this.opts.logLevel = 1
 		}
 
